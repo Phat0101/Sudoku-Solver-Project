1c864ac79cb31f18dac1906ac0e6eb913b02c4b4
Phat0101 <giaphat1206@gmail.com>
2019-10-18 23:10:25 +1100

    UI changes

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
+"use strict"
 $(document).ready(function () {
   //dynamically create sudoku table of 9x9 
   const createTable = () => {
-    $(".main").prepend("<table class = 'table'> </table>");
-    for (let i = 0; i < 9; i++) {
+    for (let i = 1; i < 10; i++) {
       $(".table").append("<tr></tr>");
       for (let j = 1; j < 10; j++) {
-        $(`tr:nth-child(${i + 1})`).append(`<td><input type= number min ="1" max = "9"</td>`);
+        $(`tr:nth-child(${i})`).append(`
+        <td id = "x${i}${j}" 
+        onmouseenter = "highlight(this.id)"
+        onmouseleave = "lowlight(this.id)">
+          <input type = number min = "1" max = "9">
+        </td>`);
       }
     }
   };
@@ -31,7 +36,41 @@ const getUserInput = () => {
   console.table(arr);
 };
 
-//Highlight the current row and column of the selected element after 1000ms
+//Perform Logical Check to ensure user provide valid input (which can be calculated)
+//In other words "Correct Sudoku problem"
+let constantly_check = true;
+const logicalCheck = (arr) => {
+
+  return true;
+}
+
+//Highlight and "lowlight:)" the current row and column of the selected element
+const highlight = (id) => {
+  let x_coordinate = parseInt(id[1]);
+  let y_coordinate = parseInt(id[2]);
+  for (let i = 1; i < 10; i++) {
+    $(`tr:nth-child(${i}) > td:nth-child(${y_coordinate})`).css("background-color", "rgba(179, 179, 179,0.3)");
+    $(`tr:nth-child(${x_coordinate}) > td:nth-child(${i})`).css("background-color", "rgba(179, 179, 179,0.3)");
+    $(`tr:nth-child(${i}) > td:nth-child(${y_coordinate}) > input`).css("background-color", "rgba(179, 179, 179,0.2)");
+    $(`tr:nth-child(${x_coordinate}) > td:nth-child(${i}) > input`).css("background-color", "rgba(179, 179, 179,0.2)");
+  }
+}
+const lowlight = (id) => {
+  let x_coordinate = parseInt(id[1]);
+  let y_coordinate = parseInt(id[2]);
+  for (let i = 1; i < 10; i++) {
+    $(`tr:nth-child(${i}) > td:nth-child(${y_coordinate})`).css("background-color", "transparent");
+    $(`tr:nth-child(${x_coordinate}) > td:nth-child(${i})`).css("background-color", "transparent");
+    $(`tr:nth-child(${i}) > td:nth-child(${y_coordinate}) > input`).css("background-color", "transparent");
+    $(`tr:nth-child(${x_coordinate}) > td:nth-child(${i}) > input`).css("background-color", "transparent");
+  }
+}
+//solve sudoku when triggered in html button "Solve"
+const solve = () => {
+  getUserInput();
+  display(solvedArray);
+};
+
 //Display output from logic.js to page
 let solvedArray = [];
 for (let i = 1; i < 10; i++) {
@@ -49,16 +88,3 @@ const display = (solvedArray) => {
     }
   }
 };
-
-//solve sudoku when triggered in html button "Solve"
-const solve = () => {
-  getUserInput();
-  display(solvedArray);
-};
-
-//Perform Logical Check to ensure user provide valid input (which can be calculated)
-//In other words "Correct Sudoku problem"
-const logicalCheck = (arr) => {
-
-  return true;
-}
